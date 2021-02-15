2089a6ad2d48baec96b26d70bd7a861ae9218e50
“Corrin <cc5948@nyu.edu>
2021-02-14 20:22:41 -0500

    build book

---
+++ _build/jupyter_execute/dplyr_library.r
@@ -0,0 +1,13 @@
+# You can ignore the warning about objects from other libraries being masked.
+library(dplyr)
+
+# The warn.conflicts=FALSE option supressess the warning about objects from other libraries being masked.
+library(dplyr, warn.conflicts=FALSE) )
+
+
+
+
+
+
+
+
---
+++ _build/jupyter_execute/ggplot_library.r
@@ -0,0 +1,15 @@
+# You can ignore the warning about objects from other libraries being masked.
+library(dplyr)
+
+# The warn.conflicts=FALSE option supressess the warning about objects from other libraries being masked.
+library(dplyr, warn.conflicts=FALSE) )
+
+
+
+
+
+
+
+
+
+
